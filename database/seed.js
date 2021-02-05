1e5de7dd9b06bb2c0279e7be35ec6b87022dde53
Danny Harmon <dannyaharmon@gmail.com>
2021-02-05 00:52:42 -0800

    update seed script

--- database/seed.js
+++ database/seed.js
@@ -12,7 +12,10 @@ db.serialize(()=>{
     id INTEGER PRIMARY KEY,
     categoryName TEXT
   )`)
-  for(let i = 1; i < 101; i++) {
+  db.run(`insert into titles(productID, title, category) values (1, 'Men\'s Retro Pile Fleece Jacket', ${faker.random.number({min:1, max: 9})})`, (err) => console.log(`inserted 1 into titles`, err))
+  db.run(`insert into titles(productID, title, category) values (2, 'Women\'s Happy Hike Studio Pants', ${faker.random.number({min:1, max: 9})})`, (err) => console.log(`inserted 1 into titles`, err))
+  db.run(`insert into titles(productID, title, category) values (3, 'Men\'s Lightweight Fjord Flannel Shirt', ${faker.random.number({min:1, max: 9})})`, (err) => console.log(`inserted 1 into titles`, err))
+  for(let i = 4; i < 101; i++) {
     db.run(`insert into titles (productID, title, category) values (${i}, '${faker.commerce.productName()}', ${faker.random.number({min:1, max: 9})})`, (err) => console.log(`inserted ${i} into titles`, err));
   }
   for(let j = 1; j < 10; j++) {
